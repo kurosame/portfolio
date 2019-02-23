62a7dc62cee94582297a3c5596721fa55f1fa4d0
kurosame <kurosame1227@gmail.com>
2019-02-23 21:06:58 +0900

    add: sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,43 @@ module.exports = {
     nav: [
       { text: 'Home', link: '/' },
       { text: 'GitHub', link: 'https://github.com/kurosame/portfolio' }
-    ]
+    ],
+    sidebar: [
+      {
+        title: 'Front-End',
+        collapsable: false,
+        children: [
+          ['/react/', 'React'],
+          ['/vuejs/', 'Vue.js'],
+          ['/angular/', 'Angular']
+        ]
+      },
+      {
+        title: 'Native-Apps',
+        collapsable: false,
+        children: [['/swift/', 'Swift'], ['/weex/', 'Weex']]
+      },
+      {
+        title: 'Back-End',
+        collapsable: false,
+        children: [
+          ['/scala/', 'Scala'],
+          ['/go/', 'Go'],
+          ['/python/', 'Python'],
+          ['/java/', 'Java'],
+          ['/php/', 'PHP']
+        ]
+      },
+      {
+        title: 'Infra',
+        collapsable: false,
+        children: [
+          ['/aws/', 'AWS‎'],
+          ['/firebase/', 'Firebase'],
+          ['/digdag/', 'Digdag']
+        ]
+      }
+    ],
+    sidebarDepth: 0
   }
 }
