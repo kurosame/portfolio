5dd6ce6c9172a495a83e31c9c3d39c52b9b2ffb2
kurosame <kurosame1227@gmail.com>
2020-03-19 21:53:30 +0900

    fix: favicon color

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,21 +4,18 @@ module.exports = {
     ['meta', { name: 'description', content: "Takayuki Hara's Portfolio" }],
     ['meta', { name: 'theme-color', content: 'white' }],
     ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', href: '/favicon.png', sizes: '72x72' }],
     [
       'link',
-      { rel: 'apple-touch-icon', href: '/favicon-ios.png', sizes: '72x72' }
+      { rel: 'apple-touch-icon', href: '/favicon.png', sizes: '114x114' }
     ],
     [
       'link',
-      { rel: 'apple-touch-icon', href: '/favicon-ios.png', sizes: '114x114' }
+      { rel: 'apple-touch-icon', href: '/favicon.png', sizes: '120x120' }
     ],
     [
       'link',
-      { rel: 'apple-touch-icon', href: '/favicon-ios.png', sizes: '120x120' }
-    ],
-    [
-      'link',
-      { rel: 'apple-touch-icon', href: '/favicon-ios.png', sizes: '144x144' }
+      { rel: 'apple-touch-icon', href: '/favicon.png', sizes: '144x144' }
     ],
     ['link', { rel: 'manifest', href: '/manifest.json' }]
   ],
